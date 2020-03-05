69f2c9a7e361d60de34506d91bc4f889e58505a4
Kein <orionmusselman@gmail.com>
2020-03-04 22:11:49 -0800

    Stack overflow? Whatever...
    Ok so it should work but it doesn't. Any siggestions?

--- src/main.cpp
+++ src/main.cpp
@@ -112,33 +112,43 @@ IParser::IParser(const char *path): img(stbi_load(path, &width, &height, &channe
 
 void IParser::parseBox(const int &i, const int &x, const int &y, const short &direction) {
 
-    std::cout << "Erm..." << std::endl;
+    // std::cout << "Erm..." << std::endl;
     if (i < size && i >= 0 && addLocation(x, y, new Node(hasNumber(i)))) {
         debug_printCoords(i);
         // Sleep(1000);
-        std::cout << "Hello?" << std::endl;
-        if (!inBox(i + moveHorzSqr)) { // East
+        // std::cout << "Hello?" << std::endl;
+        debugFile << "Pricess A" << std::endl;
+        if (inBox(i + moveHorzSqr)) { // East
             // std::cout << "Add " << moveHorzSqr << std::endl;
             debugFile << "Allowing EAST with i = " << (i + moveHorzSqr) << std::endl;
             parseBox(i + moveHorzSqr, x+1, y, 0);
         }
-        if (!inBox(i + movVertSqr)) { // South
+        debugFile << "Pricess B:" << std::endl;
+        debug_printCoords(i + movVertSqr);
+        if (inBox(i + movVertSqr)) { // South
             // std::cout << "Add " << movVertSqr << std::endl;
             debugFile << "Allowing SOUTH with i = " << (i + movVertSqr) << std::endl;
+            // std::cout << "MOV SOUTH" << std::endl;
             parseBox(i + movVertSqr, x, y+1, 1);
         }
-        if (!inBox(i - moveHorzSqr)) { // West
+        debugFile << "Pricess C" << std::endl;
+        if (inBox(i - moveHorzSqr)) { // West
             // std::cout << "Sub " << moveHorzSqr << std::endl;
             debugFile << "Allowing WEST with i = " << (i - moveHorzSqr) << std::endl;
             parseBox(i - moveHorzSqr, x-1, y, 2);
         }
-        if (!inBox(i - movVertSqr)) { // North
+        debugFile << "Pricess D" << std::endl;
+        if (inBox(i - movVertSqr)) { // North
             // std::cout << "Sub " << movVertSqr << std::endl;
             debugFile << "Allowing NORTH with i = " << (i - movVertSqr) << std::endl;
             parseBox(i - movVertSqr, x, y-1, 3);
         }
     } else {
-        std::cout << "deneid" << std::endl;
+        debugFile << "DENIED..." << std::endl;
+        // debugFile << "DENIED, i=" << i << std::endl;
+        // debug_printCoords(i);
+        // debugFile << "---" << std::endl;
+        // std::cout << "deneid" << std::endl;
     }
     // And well I guess that's the end of it
 
@@ -186,7 +196,7 @@ bool IParser::addLocation(int x, int y, Node n) {
 }
 
 bool IParser::hasNumber(int i) {
-    std::cout << "Starto" << std::endl;
+    // std::cout << "Starto" << std::endl;
     // std::cout << "------------------------------------------" << std::endl;
     // std::cout << ((i / channels) % width) << ", " << ((i / channels) / width) << std::endl;
     // std::cout << "------------------------------------------" << std::endl;
@@ -198,7 +208,7 @@ bool IParser::hasNumber(int i) {
             break;
         }
     }
-    std::cout << "~A" << std::endl;
+    // std::cout << "~A" << std::endl;
     for (;; i += channels) {
         if (i >= size) {
             return false;
@@ -207,7 +217,7 @@ bool IParser::hasNumber(int i) {
             break;
         }
     }
-    std::cout << "~B" << std::endl;
+    // std::cout << "~B" << std::endl;
     // std::cout << "------------------------------------------" << std::endl;
     // std::cout << ((i / channels) % width) << ", " << ((i / channels) / width) << std::endl;
     // std::cout << "------------------------------------------" << std::endl;
@@ -231,66 +241,75 @@ bool IParser::hasNumber(int i) {
         }
 
         if ((img[loc] << 16) | (img[loc+1] << 8) | img[loc+2] != 0xFFFFFF) {
-            std::cout << "Finisjo" << std::endl;
+            // std::cout << "Finisjo" << std::endl;
             return true;
         }
     }
-    std::cout << "Funisjo" << std::endl;
+    // std::cout << "Funisjo" << std::endl;
     return false;
 }
 
 bool IParser::inBox(const int &index) {
-    std::cout << "Run" << std::endl;
-    std::cout << "1" << std::endl;
+    // std::cout << "Run" << std::endl;
+    // std::cout << "1" << std::endl;
     for (int i = index;; i += movVert) {
         if (i >= size) {
+            debugFile << ">> end 0" << std::endl;
             return false;
         } else if (!pixelColorEq(i, 0xFF)) {
             if (std::abs(index - i) > boxDimensions) {
-                std::cout << "Return" << std::endl;
+                // std::cout << "Return" << std::endl;
+                debugFile << ">> end A" << std::endl;
                 return false;
             } else {
                 break;
             }
         }
     }
-    std::cout << "2" << std::endl;
+    // std::cout << "2" << std::endl;
     for (int i = index;; i -= movVert) {
         if (i < 0) {
+            debugFile << ">> end B" << std::endl;
             return false;
         } else if (!pixelColorEq(i, 0xFF)) {
             if (std::abs(index - i) > boxDimensions) {
+                debugFile << ">> end C" << std::endl;
                 return false;
             } else {
                 break;
             }
         }
     }
-    std::cout << "3" << std::endl;
+    // std::cout << "3" << std::endl;
     for (int i = index;; i += channels) {
         if (i >= size) {
+            debugFile << ">> end D" << std::endl;
             return false;
         } else if (!pixelColorEq(i, 0xFF)) {
             if (std::abs(index - i) > boxDimensions) {
+                debugFile << ">> end E" << std::endl;
                 return false;
             } else {
                 break;
             }
         }
     }
-    std::cout << "4" << std::endl;
+    // std::cout << "4" << std::endl;
     for (int i = index;; i -= channels) {
         if (i < 0) {
+            debugFile << ">> end F" << std::endl;
             return false;
         } else if (!pixelColorEq(i, 0xFF)) {
             if (std::abs(index - i) > boxDimensions) {
+                debugFile << ">> end G" << std::endl;
                 return false;
             } else {
                 break;
             }
         }
     }
-    std::cout << "Stop" << std::endl;
+    // std::cout << "Stop" << std::endl;
+    debugFile << ">> end H" << std::endl;
     return true;
     // return index < 0 || index >= size || (r == img[index] && b == img[index+1] && g == img[index+2]);
 }
